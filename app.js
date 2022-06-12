7e2995c7c5e6087586c8a49e6f923285b13df848
tianxu <xueqingxu0302@gmail.com>
2022-06-11 20:23:14 -0600

    test new book

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ class UI {
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.isbn}</td>
-            <td><a href="#" class= "btn btn-danger btn-sm delte">X</a></td>
+            <td><a href="#" class= "btn btn-danger btn-sm delete">X</a></td>
             `;
             list.appendChild(row);
     }
@@ -52,7 +52,19 @@ class UI {
 //Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
 //Event: Add a book
+document.querySelector("#book-form").addEventListener('submit',(e)=>
+{
+    // Prevent actual submit
+    e.preventDefault();
+    // get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+// instatiate book
+const book = new Book(title,author, isbn);
+console.log(book)
 
+})
 //Event: Remove a book
 //test OOP
 

3362fdf56e676374b559ee3c2e202d2349b5ee5f
tianxu <xueqingxu0302@gmail.com>
2022-06-11 21:38:13 -0600

    fixed the remove book alert

--- app.js
+++ app.js
@@ -40,10 +40,32 @@ class UI {
             `;
             list.appendChild(row);
     }
+
+    
+
     static deleteBook(el){
         if (el.classList.contains('delete')){
             el.parentElement.parentElement.remove();
         }
+    }
+
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`
+        div.appendChild(document.createTextNode(message))
+        const container = document.querySelector('.container')
+        const form = document.querySelector('#book-form')
+        container.insertBefore(div, form);
+        //Vanish in 3 seconds
+        setTimeout(()=> document.querySelector('.alert').remove(),3000)
+
+    }
+
+    static clearFields(){
+        document.querySelector("#title").value =""
+        document.querySelector("#author").value =""
+        document.querySelector("#isbn").value =""
+
     }
 }
 
@@ -60,11 +82,37 @@ document.querySelector("#book-form").addEventListener('submit',(e)=>
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
-// instatiate book
-const book = new Book(title,author, isbn);
-console.log(book)
 
+    // Validate
+    if(title === "" || author === "" || isbn === ""){
+        UI.showAlert('Please fill in all fields','danger')
+    } else {
+        // instatiate book
+        const book = new Book(title,author, isbn);
+        // console.log(book)
+
+        // add book to UI
+        UI.addBookToList(book)
+        // Show success message
+        UI.showAlert('Book Addes', 'success')
+        
+        // clear fields
+        UI.clearFields()
+    }
 })
 //Event: Remove a book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+        UI.deleteBook(e.target)
+     
+        console.log(e.target.parentElement.firstElementChild.textContent)
+
+        // show delete message
+        if(e.target.classList.contains('delete'))
+        UI.showAlert(`${e.target.parentElement.parentElement.firstElementChild.textContent} is deleted`, 'info')
+
+})
+
+
+
 //test OOP
 
